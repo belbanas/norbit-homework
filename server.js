50ee76f7b1c20cde99495cb25a7a3bed5c25f050
belbanas <belbanas@gmail.com>
2021-02-11 13:22:11 +0100

    add websocket server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({server: server});
+
+wss.on("connection", (ws) => {
+    console.log("A new client connected.");
+    ws.send("Welcome new client!");
+})
 
 app.get("/", (req, res) => {
     res.send("My http server is running.");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("The app is listening on port 3000.");
 });
