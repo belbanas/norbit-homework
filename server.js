7f03e1e4ef6bdafb706a55740f2f064c4c7d56f1
belbanas <belbanas@gmail.com>
2021-02-11 13:35:11 +0100

    add csv support and outputs in json

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const WebSocket = require("ws");
+const CSVToJson = require("csvtojson");
 
 const wss = new WebSocket.Server({ server: server });
 
 wss.on("connection", (ws) => {
     console.log("A new client connected.");
     ws.send("Welcome new client!");
+
+    CSVToJson().fromFile("line1.csv").then((coords) => {
+        let i = 0;
+        const interval = setInterval(() => {
+            ws.send(JSON.stringify(coords[i]));
+            i++;
+            if (i >= coords.length) {
+                clearInterval(interval);
+            }
+        }, 1000);
+    });
 });
 
 app.get("/", (req, res) => {
