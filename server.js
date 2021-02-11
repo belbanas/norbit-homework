1aaf79039b76af1d2e2979d7feaec610df15141b
belbanas <belbanas@gmail.com>
2021-02-11 13:26:25 +0100

    add websocket connection with test html

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const server = require("http").createServer(app);
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({server: server});
+const wss = new WebSocket.Server({ server: server });
 
 wss.on("connection", (ws) => {
     console.log("A new client connected.");
     ws.send("Welcome new client!");
-})
+});
 
 app.get("/", (req, res) => {
     res.send("My http server is running.");
