ec6c9731c3c18eef55ea525e0d017b2e564adcec
belbanas <belbanas@gmail.com>
2021-02-12 11:55:42 +0100

    add base structure to express.js

---
+++ express.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on("connection", (ws) => {
+    console.log("A new client connected.");
+    ws.send("Welcome new client!");
+
+});
+
+app.get("/", (req, res) => {
+    res.send("My http server is running.");
+});
+
+server.listen(3000, () => {
+    console.log("The app is listening on port 3000.");
+});
