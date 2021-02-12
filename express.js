9c8b88b07b0535b222c898e504f60deb0432ca6b
belbanas <belbanas@gmail.com>
2021-02-12 17:39:08 +0100

    add base socket.io for server and clien side

--- express.js
+++ express.js
@@ -1,22 +1,38 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ server: server });
+// const wss = new WebSocket.Server({ server: server });
 
-wss.on("connection", (ws) => {
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
     console.log("A new client connected.");
-    ws.send("SERVER: Welcome new client!");
 
-    ws.on("message", (data) => {
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
+    
+    socket.on("message", (msg) => {
+        io.emit("message2", "Hello2");
+        console.log(msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
     });
 });
+
+// wss.on("connection", (ws) => {
+//     console.log("A new client connected.");
+//     ws.send("SERVER: Welcome new client!");
+
+//     ws.on("message", (data) => {
+//         wss.clients.forEach((client) => {
+//             if (client !== ws && client.readyState === WebSocket.OPEN) {
+//                 client.send(data);
+//             }
+//         });
+//     });
+// });
 
 app.get("/", (req, res) => {
     res.send("My http server is running.");
--- test_client.js
+++ test_client.js
@@ -1,10 +1,20 @@
-const WebSocket = require("ws");
-const socket = new WebSocket("ws://localhost:3000");
+// const WebSocket = require("ws");
+// const socket = new WebSocket("ws://localhost:3000");
 
-socket.addEventListener("open", () => {
-    socket.send("Test client connected");
-})
+// socket.addEventListener("open", () => {
+//     socket.send("Test client connected");
+// })
 
-socket.addEventListener("message", (event) => {
-    console.log(event.data);
-})
+// socket.addEventListener("message", (event) => {
+//     console.log(event.data);
+// })
+
+const io = require("socket.io-client");
+
+const socket = io("ws://localhost:3000");
+
+socket.emit("message", "Hello");
+
+socket.on("message2", (msg) => {
+    console.log(msg);
+});
