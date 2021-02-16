3494ccf8fc4c821cd94dbe6b1f20915b7733bbdb
belbanas <belbanas@gmail.com>
2021-02-16 14:48:56 +0100

    fix thead, tbody tags

--- frontend_client/src/components/TracksComponent.js
+++ frontend_client/src/components/TracksComponent.js
@@ -1,26 +1,36 @@
 import React from "react";
 
 function TracksComponent(props) {
-    return <React.Fragment>
-        <h3>Recorded Tracks</h3>
-        <table>
-            <tr>
-                <th>Id</th>
-                <th>Record start</th>
-                <th>Record stop</th>
-            </tr>
-            {props.tracks.map(track => (
-                <tr key={track.id}>
-                    <td onClick={function getTrackId() {
-                        console.log(track.id)
-                        props.getPointsForTrack(track.id)
-                    }}>{track.id}</td>
-                    <td>{track.start_record}</td>
-                    <td>{track.stop_record}</td>
-                </tr>
-            ))}
-        </table>
-    </React.Fragment>;
+    return (
+        <React.Fragment>
+            <h3>Recorded Tracks</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Record start</th>
+                        <th>Record stop</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.tracks.map((track) => (
+                        <tr key={track.id}>
+                            <td
+                                onClick={function getTrackId() {
+                                    console.log(track.id);
+                                    props.getPointsForTrack(track.id);
+                                }}
+                            >
+                                {track.id}
+                            </td>
+                            <td>{track.start_record}</td>
+                            <td>{track.stop_record}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </React.Fragment>
+    );
 }
 
 export default TracksComponent;
